7611515f4bab830a4fba8bf7b0ad0b6faba7e357
Thakurji890 <tha.rupesh456@gmail.com>
2026-04-26 10:47:51 +0530

    Refactor: Extract listing controller logic

---
+++ controller/listings.js
@@ -0,0 +1,66 @@
+const Listing = require("../models/listing");
+
+module.exports.index = async (req, res) => {
+  const allListings = await Listing.find({});
+  res.render("listings/index.ejs", { allListings });
+};
+
+module.exports.newForm = async (req, res) => {
+  res.render("listings/new.ejs");
+};
+
+module.exports.showListing = async (req, res) => {
+  let { id } = req.params;
+  const listing = await Listing.findById(id)
+    .populate({
+      path: "reviews",
+      populate: {
+        path: "author",
+      },
+    })
+    .populate("owner");
+  if (!listing) {
+    req.flash("error", "Listing you requested does not exist!!!");
+    return res.redirect("/listings");
+  }
+  // console.log(listing);
+  res.render("listings/show.ejs", { listing });
+};
+
+module.exports.createListing = async (req, res) => {
+  // let { title, description, price, image, country, location } = req.body;
+  // let listing = req.body.listing;
+  let newListing = new Listing(req.body.listing);
+  newListing.owner = req.user._id;
+  await newListing.save();
+  req.flash("success", "New Listing Added Successfully!");
+  res.redirect("/listings");
+};
+
+module.exports.updateListing = async (req, res) => {
+  if (!req.body.listing) {
+    throw new ExpressError(400, "Bad Request");
+  }
+  let { id } = req.params;
+  await Listing.findByIdAndUpdate(id, { ...req.body.listing });
+  req.flash("success", "Successfully Updated listing");
+  res.redirect(`/listings/${id}`);
+};
+
+module.exports.editListing = async (req, res) => {
+  let { id } = req.params;
+  let listing = await Listing.findById(id);
+  if (!listing) {
+    req.flash("error", "Listing you requested does not exist!!!");
+    return res.redirect("/listings");
+  }
+  req.flash("success", "Successfully Edited listing");
+  res.render("listings/edit.ejs", { listing });
+};
+
+module.exports.deleteListing = async (req, res) => {
+  let { id } = req.params;
+  await Listing.findByIdAndDelete(id);
+  req.flash("success", "List Deleted Successfully!");
+  res.redirect("/listings");
+};
